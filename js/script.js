cc9b320c80b319ff80f9285eed72ee5e958ed4b7
Adam Perlis <adam.perlis@gmail.com>
2015-01-09 16:57:46 -0500

    second commit

--- js/script.js
+++ js/script.js
@@ -18,14 +18,14 @@ $(document).ready(function() {
 		behanceAPI.getProject(pid, function(p) {
 			console.log(p);
 			projects[pid] = p;
-			$(that).attr("title", p.description);
-			$("#title").append("<h1>" + p.name + "</h2>");
+			$(that).attr("title", p.description); 
+			$("#title").append("<h1>" + p.name + "</h2>"); //Titles for Projects
 			for(var i=0; i<p.modules.length; i++) {
 				if(p.modules[i].type == "text") {
-					$("#description").append("<p>'"+ p.modules[i].text +"'</p>");
+					$("#description").append("<p>'"+ p.modules[i].text +"'</p>"); //Descriptions for Projects
 				}else if (p.modules[i].type == "image") {
-					$(that).append("<img class='thumbnail' src='"+ p.modules[i].src +"'/>");
-				} else if (p.modules[i].type == "embed") {
+					$(that).append("<img class='thumbnail' src='"+ p.modules[i].src +"'/>"); //Images for Projects
+				} else if (p.modules[i].type == "embed") { //Embeds for projects
 					$(that).append(p.modules[i].embed);
 				}
 			}
